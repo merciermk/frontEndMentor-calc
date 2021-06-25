62de03affb5b66c0c1a5a5bdbc826c8bf68ae451
m.mercier <mmercier@pc-m.mercier>
2021-06-25 11:11:07 +0200

    calc using vuex/typescript

---
+++ src/components/Calc.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="#calculette">
+    <div id="#calcHeader">
+      <h4>calc</h4>
+    </div>
+    <div id="resultat">
+      {{ affichage }}
+    </div>
+    <div id="interface">
+      <button class="small-button" value="7" @click="userInput($event)">7</button>
+      <button class="small-button" value="8" @click="userInput($event)">8</button>
+      <button class="small-button" value="9" @click="userInput($event)">9</button>
+      <button class="small-button del" @click="deleteActions">DEL</button>
+
+      <button class="small-button" value="4" @click="userInput($event)">4</button
+      ><button class="small-button" value="5" @click="userInput($event)">5</button
+      ><button class="small-button" value="6" @click="userInput($event)">6</button
+      ><button class="small-button addition" value="+" @click="userInput($event)">+</button>
+
+      <button class="small-button" value="1" @click="userInput($event)">1</button
+      ><button class="small-button" value="2" @click="userInput($event)">2</button
+      ><button class="small-button" value="3" @click="userInput($event)">3</button
+      ><button class="small-button soustraction" value="-" @click="userInput($event)" >-</button>
+
+      <button class="small-button point" value="."  @click="userInput($event)">.</button
+      ><button class="small-button" value="0"  @click="userInput($event)">0</button
+      ><button class="small-button division" value="div"  @click="userInput($event)">/</button
+      ><button class="small-button multiplication" value="*" @click="userInput($event)">x</button>
+
+      <button class="large-button reset" value="reset" @click="resetAll">RESET</button>
+      <button class="large-button egale validation" value="enter" @click="result">=</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { Action } from 'vuex-class'
+import { mapGetters } from 'vuex'
+import store from '../store/index'
+
+@Component({
+
+  computed: {
+    ...mapGetters(['affichage'])
+  }
+})
+export default class Calc extends Vue {
+  @Action('result')
+  result!: void
+
+  @Action('resetAll')
+  resetAll!: void
+
+  @Action('deleteActions')
+  deleteActions!: void
+
+  @Action('addLastInput')
+  addLastInput!: void
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  userInput (event): void {
+    console.log(typeof event)
+    store.dispatch('addLastInput', event.target.value)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  color: red;
+}
+#interface {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 400px;
+  height: 400px;
+  background-color: hsl(223, 31%, 20%);
+}
+.small-button {
+  margin: 15px;
+  width: 60px;
+  height: 30px;
+}
+.large-button {
+  margin: 15px;
+  width: 150px;
+  height: 30px;
+}
+</style>
+
+function dispatch(arg0: string, value: any) {
+  throw new Error('Function not implemented.')
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,12 +4,51 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+
   state: {
+    vueXUserInput: '0'
   },
+
   mutations: {
+    addToArray (state, input) {
+      if (state.vueXUserInput === '0') {
+        state.vueXUserInput = input
+      } else {
+        state.vueXUserInput += input
+      }
+    },
+    deleteLastInArray (state) {
+      state.vueXUserInput = state.vueXUserInput.substr(0, state.vueXUserInput.length - 1)
+    },
+    eval (state) {
+      /* eslint no-eval: 0 */
+      const tampon = eval(state.vueXUserInput)
+      state.vueXUserInput = tampon.toString()
+    },
+    deleteAll (state) {
+      state.vueXUserInput = '0'
+    }
   },
+
   actions: {
+    deleteActions ({ commit }) {
+      commit('deleteLastInArray')
+    },
+    addLastInput ({ commit }, input) {
+      console.log(input)
+      commit('addToArray', input)
+    },
+    result ({ commit }) {
+      commit('eval')
+    },
+    resetAll ({ commit }) {
+      commit('deleteAll')
+    }
   },
-  modules: {
+
+  getters: {
+    affichage (state) {
+      return state.vueXUserInput
+    }
   }
 })
